401ee4e461ebd6db1984f27adbbe664ca2a9a842
lupo tobi <daniellupo30@gmail.com>
2025-01-19 17:00:17 -0800

    add base path for deployment and include build output files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/google-gemini/',
   plugins: [react()],
   build: {
     outDir: 'build',
